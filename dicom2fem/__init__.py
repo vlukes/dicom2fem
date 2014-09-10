4daf98fb896e52ef08cfb85e8ae9fab6c6d6c26f
M. Jirik <miroslav.jirik@gmail.com>
2014-09-10 12:04:35 +0200

    installation using setuptools

---
+++ dicom2fem/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['base', 'dicom2fem', 'genfem_base', 'ioutils', 'marching_cubes',
+           'mesh', 'meshio', 'seg2fem', 'viewer', 'vtk2stl']
+import base, dicom2fem, genfem_base, ioutils, marching_cubes
+import mesh, meshio, seg2fem, viewer, vtk2stl
