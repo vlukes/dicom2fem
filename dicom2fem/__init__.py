f6ffe63a49ef0ff222fb5f595009d8f6fe196b4a
Vladimir Lukes <vlukes@kme.zcu.cz>
2021-08-23 12:58:02 +0200

    update package info

--- dicom2fem/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['dicom2fem', 'genfem_base', 'marching_cubes',
-           'seg2fem']
-#    , 'viewer']
-
-import dicom2fem
-import genfem_base
-import marching_cubes
-import seg2fem
-# import viewer
--- setup.py
+++ setup.py
@@ -1,79 +1,23 @@
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from os import path
+import setuptools
 
-here = path.abspath(path.dirname(__file__))
-setup(
+setuptools.setup(
     name='dicom2fem',
     description='Generation of finite element meshes from DICOM images',
     long_desctioption="Generation of finite element meshes using computed " +
          "tomography scans. Segmentation is based on the graph cut algorithm.",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='2.0',
+    version='2.0.0',
     url='https://github.com/vlukes/dicom2fem',
     author='Vladimir Lukes',
     author_email='vlukes@kme.zcu.cz',
     license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Development Status :: 4',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ],
-
-    # What does your project relate to?
     keywords='fem dicom',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['dist',  'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    # install_requires=['numpy'],
-    # dependency_links=['https://github.com/mjirik/gco_python'],
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    package_dir={"": "dicom2fem"},
+    packages=setuptools.find_packages(where="dicom2fem"),
 )
