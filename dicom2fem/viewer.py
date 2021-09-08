8d31ac4e2416aff7fb5c51fc0914b901545fdaeb
Vladimir Lukes <vlukes@kme.zcu.cz>
2021-09-08 11:02:34 +0200

    migrate PyQt4 -> PyQt5, fix format

--- dicom2fem/viewer.py
+++ dicom2fem/viewer.py
@@ -10,9 +10,12 @@ $ viewer.py -f head.vtk
 from optparse import OptionParser
 import sys
 
-from PyQt4.QtGui import QApplication, QDialog, QGridLayout, QPushButton 
-from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 import vtk
+from PyQt5.QtWidgets import QGridLayout, QDialog, QPushButton, QApplication
+import vtk.qt
+vtk.qt.QVTKRWIBase = 'QGLWidget'
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
 
 class QVTKViewer(QDialog):
     """
@@ -28,7 +31,7 @@ class QVTKViewer(QDialog):
         btn_close = QPushButton("close", self)
         btn_close.clicked.connect(self.close)
         grid.addWidget(btn_close, 1, 0, 1, 1)
-        
+
         self.setLayout(grid)
         self.setWindowTitle('VTK Viewer')
         self.show()
@@ -45,7 +48,7 @@ class QVTKViewer(QDialog):
 
         QDialog.__init__(self)
         self.initUI()
-        
+
         ren = vtk.vtkRenderer()
         self.vtkWidget.GetRenderWindow().AddRenderer(ren)
         iren = self.vtkWidget.GetRenderWindow().GetInteractor()
@@ -56,30 +59,30 @@ class QVTKViewer(QDialog):
         reader.Update()
 
         # VTK surface
-        surface=vtk.vtkDataSetSurfaceFilter()
-        surface.SetInput(reader.GetOutput())
+        surface = vtk.vtkDataSetSurfaceFilter()
+        surface.SetInputConnection(reader.GetOutputPort())
         surface.Update()
 
         mapper = vtk.vtkDataSetMapper()
-        mapper.SetInput(surface.GetOutput())
+        mapper.SetInputConnection(surface.GetOutputPort())
 
         actor = vtk.vtkActor()
         actor.SetMapper(mapper)
         actor.GetProperty().EdgeVisibilityOn()
-        actor.GetProperty().SetEdgeColor(1,1,1)
+        actor.GetProperty().SetEdgeColor(1, 1, 1)
         actor.GetProperty().SetLineWidth(0.5)
         ren.AddActor(actor)
 
         # annot. cube
-        axesActor = vtk.vtkAnnotatedCubeActor();
+        axesActor = vtk.vtkAnnotatedCubeActor()
         axesActor.SetXPlusFaceText('R')
         axesActor.SetXMinusFaceText('L')
         axesActor.SetYMinusFaceText('H')
         axesActor.SetYPlusFaceText('F')
         axesActor.SetZMinusFaceText('A')
         axesActor.SetZPlusFaceText('P')
-        axesActor.GetTextEdgesProperty().SetColor(1,1,0)
-        axesActor.GetCubeProperty().SetColor(0,0,1)
+        axesActor.GetTextEdgesProperty().SetColor(1, 1, 0)
+        axesActor.GetCubeProperty().SetColor(0, 0, 1)
         self.axes = vtk.vtkOrientationMarkerWidget()
         self.axes.SetOrientationMarker(axesActor)
         self.axes.SetInteractor(iren)
@@ -88,12 +91,14 @@ class QVTKViewer(QDialog):
 
         ren.ResetCamera()
         iren.Initialize()
-        
+
+
 usage = '%prog [options]\n' + __doc__.rstrip()
 help = {
     'in_file': 'input VTK file with unstructured mesh',
 }
-  
+
+
 def main():
     parser = OptionParser(description='Simple VTK Viewer')
     parser.add_option('-f','--filename', action='store',
@@ -106,7 +111,9 @@ def main():
 
     app = QApplication(sys.argv)
     viewer = QVTKViewer(options.in_filename)
+    viewer.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
