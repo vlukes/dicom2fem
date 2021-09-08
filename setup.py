b60b29aae22ccfe8142a285d598cbff2364ac098
Vladimir Lukes <vlukes@kme.zcu.cz>
2021-09-08 10:49:21 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='dicom2fem',
     description='Generation of finite element meshes from DICOM images',
-    long_desctioption="Generation of finite element meshes using computed " +
+    long_description="Generation of finite element meshes using computed " +
          "tomography scans. Segmentation is based on the graph cut algorithm.",
     version='2.0.0',
     url='https://github.com/vlukes/dicom2fem',
@@ -11,13 +11,12 @@ setuptools.setup(
     author_email='vlukes@kme.zcu.cz',
     license='MIT',
     classifiers=[
-        'Development Status :: 4',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     keywords='fem dicom',
-    package_dir={"": "dicom2fem"},
-    packages=setuptools.find_packages(where="dicom2fem"),
+    packages=['dicom2fem'],
 )
